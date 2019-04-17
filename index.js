52178c0251f6c7a49e9813fe7b7ab1a41ff07456
Pavel Valentov <pavel@valentov.ru>
2019-04-17 12:22:05 +0300

    3st

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
+const Quaternion = require('quaternion');
 const dgram = require('dgram');
+const ROSLIB = require('roslib');
+const OSC = require('osc-js');
 
 // Connecting to ROS
 // -----------------
@@ -73,10 +76,101 @@ Object.size = function(obj) {
 
 // Get the size of an object
 
+function roughSizeOfObject( object ) {
+
+  var objectList = [];
+  var stack = [ object ];
+  var bytes = 0;
+
+  while ( stack.length ) {
+    var value = stack.pop();
+
+    if ( typeof value === 'boolean' ) {
+      bytes += 4;
+    }
+    else if ( typeof value === 'string' ) {
+      bytes += value.length * 2;
+    }
+    else if ( typeof value === 'number' ) {
+      bytes += 8;
+    }
+    else if
+    (
+      typeof value === 'object'
+      && objectList.indexOf( value ) === -1
+    )
+    {
+      objectList.push( value );
+
+      for( var i in value ) {
+        stack.push( value[ i ] );
+      }
+    }
+  }
+  return bytes;
+}
+
+
 tfClient.subscribe('body', function(tf) {
-  console.log(tf);
-  // const size = Object.size(tf);
+  // console.log(tf);
+  // const size = roughSizeOfObject(tf);
+  // console.log(typeof tf === 'object', size);
   const JS = JSON.stringify(tf);
   const size = JS.length;
-  client.send(JS,0, size, 9988, '127.0.0.1');
+  console.log(JS);
+  console.log(typeof tf === 'object', size);
+//  client.send(JS,0, size, 8888, '192.168.77.40');
+// client.send(JS,0, size, 8888, '192.168.77.20');
+
+
+  if (typeof tf === 'object' && tf.hasOwnProperty('rotation')) {
+    const rotation = new Quaternion(tf.rotation);
+    rotation.from
+    console.log('Quaternion ', rotation);
+
+    // TRANSLATION
+    let osc = new OSC();
+    let message = new OSC.Message('/translation/x', tf.translation.x, 'x');
+    // osc.send(message);
+    let binary = message.pack()
+    client.send(new Buffer(binary), 0, binary.byteLength, 4444, '192.168.77.18');
+
+    osc = new OSC();
+    message = new OSC.Message('/translation/y', tf.translation.y, 'y');
+    // osc.send(message);
+    binary = message.pack()
+    client.send(new Buffer(binary), 0, binary.byteLength, 4444, '192.168.77.18');
+
+    osc = new OSC();
+    message = new OSC.Message('/translation/z', tf.translation.z, 'z');
+    // osc.send(message);
+    binary = message.pack()
+    client.send(new Buffer(binary), 0, binary.byteLength, 4444, '192.168.77.18');
+
+
+
+    //   ROTATION
+    osc = new OSC();
+    message = new OSC.Message('/rotation/w', tf.rotation.w, 'w');
+    binary = message.pack()
+    client.send(new Buffer(binary), 0, binary.byteLength, 4444, '192.168.77.18');
+
+    osc = new OSC();
+    message = new OSC.Message('/rotation/x', tf.rotation.x, 'x');
+    binary = message.pack()
+    client.send(new Buffer(binary), 0, binary.byteLength, 4444, '192.168.77.18');
+
+    osc = new OSC();
+    message = new OSC.Message('/rotation/y', tf.rotation.y, 'y');
+    binary = message.pack()
+    client.send(new Buffer(binary), 0, binary.byteLength, 4444, '192.168.77.18');
+
+    osc = new OSC();
+    message = new OSC.Message('/rotation/z', tf.rotation.z, 'z');
+    binary = message.pack()
+    client.send(new Buffer(binary), 0, binary.byteLength, 4444, '192.168.77.18');
+
+
+  }
+  // client.send(tf,0, size, 8888, '192.168.77.255');
 });
